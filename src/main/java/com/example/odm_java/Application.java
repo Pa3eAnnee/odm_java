8e77bd71ee7618d9932e5c490b31e6b6bc1e8726
vanande <vkhatchatrian1@myges.fr>
2024-06-14 00:01:53 +0100

    add panel

---
+++ src/main/java/com/example/odm_java/Application.java
@@ -0,0 +1,26 @@
+package com.example.odm_java;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+import java.io.IOException;
+
+public class Application extends javafx.application.Application {
+    double x, y = 0;
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader root = new FXMLLoader(Application.class.getResource("main.fxml"));
+        Scene scene = new Scene(root.load(), 560, 400);
+        stage.initStyle(StageStyle.UNDECORATED);
+
+        stage.setTitle("App");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
