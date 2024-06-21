a644f2a83496d8de26e384a255c898ce414b02ef
vanande <vkhatchatrian1@myges.fr>
2024-06-22 01:53:03 +0200

    add event on click

--- src/main/java/com/example/odm_java/Controller.java
+++ src/main/java/com/example/odm_java/Controller.java
@@ -1,14 +1,33 @@
 package com.example.odm_java;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
 
 public class Controller {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Button membresButton;
+
+    @FXML
+    private Button planningButton;
+
+    @FXML
+    private Button listeDeCourseButton;
+
+    @FXML
+    private Button projetsButton;
+
+    @FXML
+    public void handleButtonClick(ActionEvent event) {
+        Button clickedButton = (Button) event.getSource();
+        String buttonText = clickedButton.getText();
+
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Button Clicked");
+        alert.setHeaderText(null);
+        alert.setContentText(buttonText);
+        alert.showAndWait();
     }
 }
